6ad7e2559d8dfbf4d7fd35080ec7a7b9ae6b7196
Peter de Croos <cultofmetatron@aumlogic.com>
2018-01-20 00:04:31 -0500

    stubbed out account controllers

---
+++ frontend/src/pages/account/Signup.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Signup
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+<style>
+
+</style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,15 +1,34 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Profile from '@/pages/account/Profile'
+import Signup from '@/pages/account/Signup'
+import Signin from '@/pages/account/Signin'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/account/profile',
+      name: 'Profile',
+      component: Profile
+    },
+    {
+      path: '/account/signup',
+      name: 'Signup',
+      component: Signup
+    },
+    {
+      path: '/account/signin',
+      name: 'Signin',
+      component: Signin
     }
   ]
 })
