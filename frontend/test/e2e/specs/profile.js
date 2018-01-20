6ede2fa7e0f4bb817db370c16af1213d89c29293
Peter de Croos <cultofmetatron@aumlogic.com>
2018-01-20 03:21:52 -0500

    added vuetify

---
+++ frontend/test/e2e/specs/profile.js
@@ -0,0 +1,20 @@
+
+module.exports = {
+  'open profile page test': function (browser) {
+    // automatically uses dev Server port from /config.index.js
+    // default: http://localhost:8080
+    // see nightwatch.conf.js
+    const path = 'account/profile'
+    const devServer = `${browser.globals.devServerURL}/${path}`
+
+    browser
+    .url(devServer)
+    .waitForElementVisible('#app', 5000)
+    .getLog('browser', (result) => {
+        console.log(result)
+    })
+    .assert.elementPresent('h1')
+    .assert.containsText('h1', 'Profile')
+    .end()
+  }
+}
---
+++ frontend/test/e2e/specs/signup.js
@@ -0,0 +1,20 @@
+
+module.exports = {
+  'open signup page test': function (browser) {
+    // automatically uses dev Server port from /config.index.js
+    // default: http://localhost:8080
+    // see nightwatch.conf.js
+    const path = 'account/signup'
+    const devServer = `${browser.globals.devServerURL}/${path}`
+
+    browser
+    .url(devServer)
+    .waitForElementVisible('#app', 5000)
+    .getLog('browser', (result) => {
+        console.log(result)
+    })
+    .assert.elementPresent('h1')
+    .assert.containsText('h1', 'Signup')
+    .end()
+  }
+}
